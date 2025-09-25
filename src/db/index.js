894018cda3fc43438b3f0fed155d252184f94700
shubham7648 <shubhams7648@gmail.com>
2025-09-25 13:57:22 +0530

    connect to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+const connectDB = async () =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n mongosooe database connected!! ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGOOSE ERROR DB FAILED!!",error)
+    }
+}
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+    path: "./.env" 
+}); 
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+
+// const app = express();
+
+
+// (async ()=> {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`port is lsitening to ${process.env.PORT}`)
+//         })
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();                                                                     
